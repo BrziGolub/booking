11cc4e45b394d980bdc94fb04520c7ee57703142
Petar Kovacevic <petarkovacevic58@gmail.com>
2023-03-05 22:05:20 +0100

    Added a few models

---
+++ Booking/Booking/Model/Location.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Booking.Serializer;
+
+namespace Booking.Model
+{
+	internal class Location : ISerializable
+	{
+		public int Id { get; set; }
+		public string State { get; set; }
+		public string City { get; set; }
+
+		public Location() { }
+
+		public Location(string state, string city)
+		{
+			State = state;
+			City = city;
+		}
+
+		public string[] ToCSV()
+		{
+			string[] csvValues = { Id.ToString(), State, City};
+			return csvValues;
+		}
+
+		public void FromCSV(string[] values)
+		{
+			Id = Convert.ToInt32(values[0]);
+			State = values[1];
+			City = values[2];
+		}
+	}
+}
---
+++ Booking/Booking/Model/Tour.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Booking.Serializer;
+
+namespace Booking.Model
+{
+	internal class Tour : ISerializable
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public Location Location { get; set; }
+
+		public string Description { get; set; }
+
+		public string Language { get; set; } // enum
+
+		public int MaxNumberOfGuests { get; set; }
+
+		public List<string> destinations { get; set; }
+
+		public DateTime DateTime { get; set; } // rename
+
+		public int Duration { get; set; } // rename
+
+		public List<string> pictures { get; set; }
+
+		public Tour() 
+		{
+			destinations = new List<string>();
+			pictures = new List<string>();
+		}
+
+		public Tour( string name, Location location, string desc, string lang, int maxNum, DateTime dt, int duration)
+		{
+			Name = name;
+			Location = location;
+			Description = desc;
+			Language = lang;
+			MaxNumberOfGuests = maxNum;
+			DateTime = dt;
+			Duration = duration;
+			destinations = new List<string>();
+			pictures = new List<string>();
+		}
+
+		public string[] ToCSV()
+		{
+			string[] csvValues = { Id.ToString(), Name, Location.Id.ToString(), Description, Language, MaxNumberOfGuests.ToString(), DateTime.ToString(), Duration.ToString()};
+			return csvValues;
+		}
+
+		public void FromCSV(string[] values)
+		{
+			Id = Convert.ToInt32(values[0]);
+			Name = values[1];
+			Location.Id = Convert.ToInt32(values[2]);
+			Description = values[3];
+			Language = values[4];
+			MaxNumberOfGuests = Convert.ToInt32(values[5]);
+			//DateTime = 
+			Duration = Convert.ToInt32(values[7]);
+		}
+	}
+}
