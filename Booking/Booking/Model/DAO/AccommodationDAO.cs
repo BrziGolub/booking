c2084670693201cb0776353808a135c4b1af3090
milovicaleksandar001 <milovicaleksandar001@gmail.com>
2023-03-14 20:26:38 +0100

    Started making the option to make new accommodations

--- Booking/Booking/Model/DAO/AccommodationDAO.cs
+++ Booking/Booking/Model/DAO/AccommodationDAO.cs
@@ -1,4 +1,5 @@
-﻿using Booking.Repository;
+﻿using Booking.Observer;
+using Booking.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,17 +8,22 @@ using System.Threading.Tasks;
 
 namespace Booking.Model.DAO
 {
-    public class AccommodationDAO
+    public class AccommodationDAO : ISubject
     {
         //Add observer?
 
+        private readonly List<IObserver> observers;
         private readonly AccommodationRepository _accommodationRepository;
-
         private List<Accommodation> accommodations;
+
+        private LocationDAO locationDAO;
 
         public AccommodationDAO()
         {
             _accommodationRepository = new AccommodationRepository();
+            observers = new List<IObserver>();
+            accommodations = new List<Accommodation>();
+            locationDAO = new LocationDAO();
             Load();
         }
 
@@ -34,6 +40,24 @@ namespace Booking.Model.DAO
         public List<Accommodation> GetAll()
         {
             return accommodations;
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Unsubscribe(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update();
+            }
         }
 
     }
