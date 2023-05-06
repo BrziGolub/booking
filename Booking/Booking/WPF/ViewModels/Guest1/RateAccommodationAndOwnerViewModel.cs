8bd07ed786d563e4fff1951feaab08a5f3c42173
zelick <116888497+zelick@users.noreply.github.com>
2023-05-06 02:09:33 +0200

    added pictures for rate accommodation

--- Booking/Booking/WPF/ViewModels/Guest1/RateAccommodationAndOwnerViewModel.cs
+++ Booking/Booking/WPF/ViewModels/Guest1/RateAccommodationAndOwnerViewModel.cs
@@ -40,7 +40,14 @@ namespace Booking.WPF.ViewModels.Guest1
 
         public NavigationService NavigationService { get; set; }
 
-     
+        List<string> imagePaths = new List<string>();
+        int currentImageIndex = -1;
+
+        public RelayCommand AddPicture { get; set; }
+        public RelayCommand RemovePicture { get; set; }
+        public RelayCommand NextPicture { get; set; }
+        public RelayCommand PreviousPicture { get; set; }
+
 
         private int _cleaness;
         public int Cleaness
@@ -82,14 +89,64 @@ namespace Booking.WPF.ViewModels.Guest1
                     OnPropertyChanged();
                 }
             }
-    
+        }
 
 
-      
+
+        public List<string> _pictures;
+        public List<string> Pictures
+        {
+            get => _pictures;
+            set
+            {
+                if (_pictures != value)
+                {
+                    _pictures = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string _tbPictures;
+        public string TbPictures
+        {
+            get => _tbPictures;
+            set
+            {
+                if (_tbPictures != value)
+                {
+                    _tbPictures = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private BitmapImage _imageSlideshowSource;
+        public BitmapImage ImageSlideshowSource
+        {
+            get { return _imageSlideshowSource; }
+            set
+            {
+                _imageSlideshowSource = value;
+                OnPropertyChanged(nameof(ImageSlideshowSource));
+            }
+        }
+
+        private Visibility _buttonNextVisibility = Visibility.Visible;
+        public Visibility ButtonNextVisibility
+        {
+            get { return _buttonNextVisibility; }
+            set { _buttonNextVisibility = value; OnPropertyChanged(); }
+        }
+
+        private Visibility _buttonPreviousVisibility = Visibility.Visible;
+        public Visibility ButtonPreviousVisibility
+        {
+            get { return _buttonPreviousVisibility; }
+            set { _buttonPreviousVisibility = value; OnPropertyChanged(); }
+        }
 
 
-        List<string> imagePaths = new List<string>();
-        int currentImageIndex = -1;
         public RelayCommand Button_Click_Subbmit { get; set; }
         public RelayCommand Button_Click_Plus { get; set; }
 
@@ -164,13 +221,36 @@ namespace Booking.WPF.ViewModels.Guest1
 
             NavigationService = navigationService;
             Button_Click_Subbmit = new RelayCommand(ButtonSubbmit);
-            //Button_Click_Plus = new RelayCommand(ButtonPlus);
+
+            AddPicture = new RelayCommand(ButtonAddPicture);
+            RemovePicture = new RelayCommand(ButtonRemovePicture);
+
+            NextPicture = new RelayCommand(buttonNext_Click);
+            PreviousPicture = new RelayCommand(buttonPrevious_Click);
+           
             Comment = String.Empty;
             Courtesy = 0;
             Cleaness = 0;
             setComboBoxes();
-           
+            NextPreviousPhotoButtonsVisibility();
+
         }
+
+        private void NextPreviousPhotoButtonsVisibility()
+        {
+            if (imagePaths.Count == 0)
+            {
+                ImageSlideshowSource = null;
+                ButtonNextVisibility = Visibility.Collapsed;
+                ButtonPreviousVisibility = Visibility.Collapsed;
+            }
+            else
+            {
+                ButtonNextVisibility = Visibility.Visible;
+                ButtonPreviousVisibility = Visibility.Visible;
+            }
+        }
+
 
         public void setComboBoxes()
         {
@@ -216,30 +296,96 @@ namespace Booking.WPF.ViewModels.Guest1
             notificationManager.Show(content, areaName: "WindowArea", expirationTime: TimeSpan.FromSeconds(5));
         }
 
+        private void ButtonAddPicture(object param)
+        {
+            System.Windows.Forms.OpenFileDialog dialog = new System.Windows.Forms.OpenFileDialog();
+            dialog.Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
 
-        /*  private void MakePicture(GuestsAccommodationImages Picture)
-          {
-              Picture.Url = Pictures;
-              Picture.Guest.Id = AccommodationReservationService.SignedFirstGuestId;
-              Picture.Grade.Id = accommodationAndOwnerGrade.Id;
-              accommodationAndOwnerGrade.Images.Add(Picture);
-          }
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string imagePath = dialog.FileName;
+                imagePaths.Add(imagePath);
+                currentImageIndex = imagePaths.Count - 1;
+                ImageSlideshowSource = new BitmapImage(new Uri(imagePath));
+                TbPictures = imagePath;
+            }
 
-          private void ButtonPlus(object param)
-          {
-              GuestsAccommodationImages Picture = new GuestsAccommodationImages();
+            if (TbPictures != "")
+            {
+                GuestsAccommodationImages Images = new GuestsAccommodationImages();
+                Images.Url = TbPictures;
+                accommodationAndOwnerGrade.Images.Add(Images);
+                NextPreviousPhotoButtonsVisibility();
+            }
+            else
+            {
+                MessageBox.Show("Photo URL can't be empty!");
+            }
+        }
 
-              MakePicture(Picture);
-              GuestsAccommodationImagesService.Create(Picture);
+        private void ButtonRemovePicture(object param)
+        {
+            TourImage image = new TourImage
+            {
+                Url = TbPictures
+            };
+            ImageSlideshowSource = null;
+            imagePaths.Remove(image.Url);
 
-              Pictures = string.Empty;
-          }*/
+            int indexToRemove = -1;
+            for (int i = 0; i < accommodationAndOwnerGrade.Images.Count; i++)
+            {
+                if (accommodationAndOwnerGrade.Images[i].Url == image.Url)
+                {
+                    indexToRemove = i;
+                    break;
+                }
+            }
 
-     
+            if (indexToRemove != -1)
+            {
+                accommodationAndOwnerGrade.Images.RemoveAt(indexToRemove);
 
-      
+                if (accommodationAndOwnerGrade.Images.Count == 0)
+                {
+                    currentImageIndex = -1;
+                    TbPictures = "";
+                    ImageSlideshowSource = null;
+                }
+                else
+                {
+                    currentImageIndex = (currentImageIndex - 1 + accommodationAndOwnerGrade.Images.Count) % accommodationAndOwnerGrade.Images.Count;
+
+                    ImageSlideshowSource = new BitmapImage(new Uri(accommodationAndOwnerGrade.Images[currentImageIndex].Url));
+                    TbPictures = accommodationAndOwnerGrade.Images[currentImageIndex].Url;
+                }
+            }
+
+            NextPreviousPhotoButtonsVisibility();
+        }
 
 
+        private void buttonPrevious_Click(object param)
+        {
+            if (currentImageIndex > 0)
+            {
+                currentImageIndex--;
+                string imagePath = imagePaths[currentImageIndex];
+                ImageSlideshowSource = new BitmapImage(new Uri(imagePath));
+                TbPictures = imagePath;
+            }
+        }
+
+        private void buttonNext_Click(object param)
+        {
+            if (currentImageIndex < imagePaths.Count - 1)
+            {
+                currentImageIndex++;
+                string imagePath = imagePaths[currentImageIndex];
+                ImageSlideshowSource = new BitmapImage(new Uri(imagePath));
+                TbPictures = imagePath;
+            }
+        }
 
     }
 }
