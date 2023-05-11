4c6d2bac8224c4e6663b6f22dc50d80847efc7c5
Petar Kovacevic <petarkovacevic58@gmail.com>
2023-05-11 19:19:45 +0200

    changed look and way of navigation of showImages window

--- Booking/Booking/WPF/ViewModels/Guest2/ShowTourImagesViewModel.cs
+++ Booking/Booking/WPF/ViewModels/Guest2/ShowTourImagesViewModel.cs
@@ -1,36 +1,105 @@
 ﻿using Booking.Commands;
 using Booking.Model;
 using Booking.Model.Images;
-using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace Booking.WPF.ViewModels.Guest2
 {
-	public class ShowTourImagesViewModel
-	{
-		private readonly Window _window;
+    public class ShowTourImagesViewModel : INotifyPropertyChanged
+    {
+        private readonly Window _window;
 
-		public ObservableCollection<TourImage> Images { get; set; }
-		public Tour Tour { get; set; }
-		public string Location { get; set; }
-		public RelayCommand Button_Click_Close { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
 
-		public ShowTourImagesViewModel(Window window, Tour tour)
-		{
-			_window = window;
-			Tour = tour;
-			Location = tour.Location.State + ", " + tour.Location.City;
-			Images = new ObservableCollection<TourImage>(tour.Images);
-			Button_Click_Close = new RelayCommand(ButtonClose);
-		}
-		private void ButtonClose(object param)
-		{
-			_window.Close();
-		}
-	}
+        public List<TourImage> Images { get; set; }
+        public Tour Tour { get; set; }
+        public string Location { get; set; }
+
+        private int _index;
+        private int _listSize;
+
+        private string _image;
+        public string ImageSource
+        {
+            get => _image;
+            set
+            {
+                if (_image != value)
+                {
+                    _image = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _text;
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public RelayCommand Button_Click_Close { get; set; }
+        public RelayCommand Button_Click_Next { get; set; }
+        public RelayCommand Button_Click_Prev { get; set; }
+
+        public ShowTourImagesViewModel(Window window, Tour tour)
+        {
+            _window = window;
+            Tour = tour;
+            Location = tour.Location.State + ", " + tour.Location.City;
+            Images = tour.Images;
+
+            _index = 0;
+            _listSize = Images.Count;
+
+            UpdateImage();
+            InitializeCommands();
+        }
+
+        private void InitializeCommands()
+        {
+            Button_Click_Close = new RelayCommand(ButtonClose);
+            Button_Click_Next = new RelayCommand(ButtonNext);
+            Button_Click_Prev = new RelayCommand(ButtonPrev);
+        }
+
+        private void ButtonClose(object param)
+        {
+            _window.Close();
+        }
+
+        private void ButtonNext(object param)
+        {
+            _index = (_index + 1) % _listSize;
+            UpdateImage();
+        }
+
+        private void ButtonPrev(object param)
+        {
+            _index = (_listSize + _index - 1) % _listSize;
+            UpdateImage();
+        }
+
+        private void UpdateImage()
+        {
+            ImageSource = Images[_index].Url;
+            Text = _index + 1 + "/" + _listSize;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
