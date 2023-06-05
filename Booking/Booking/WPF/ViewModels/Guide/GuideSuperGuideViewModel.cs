d11523d4ab88650bb0edf5882c89af82ecbf0bbf
z1cLa <miljanlazic@gmail.com>
2023-06-05 23:01:57 +0200

    Added shortcuts and SuperGuide window and viewmodel

---
+++ Booking/Booking/WPF/ViewModels/Guide/GuideSuperGuideViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Booking.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Booking.WPF.ViewModels.Guide
+{
+    public class GuideSuperGuideViewModel
+    {
+        public RelayCommand Close { get; set; }
+        private readonly Window _window;
+        public GuideSuperGuideViewModel(Window window) 
+        {
+            _window = window;
+            SetCommands();
+        }
+
+        
+
+        public void SetCommands()
+        {
+            Close = new RelayCommand(ButtonClose);
+        }
+
+        private void ButtonClose(object param)
+        {
+            CloseWindow();
+        }
+        private void CloseWindow()
+        {
+            _window.Close();
+        }
+    }
+}
